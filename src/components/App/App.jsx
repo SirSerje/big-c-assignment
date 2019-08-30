f1c4c22f7695b87b4d78a554bbcd29fe5d9f865a
SirSerje <sir.serje@gmail.com>
2019-08-30 10:18:58 +0300

    update styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ import Category from '../Category';
 import CartComponent from '../CartComponent';
 import Product from '../Product';
 import CartPopup from '../CartPopup'
-import OutsideDetector from "../../OutsideDetector";
+import OutsideDetector from "../OutsideDetector";
 
 class App extends React.PureComponent {
   constructor(props) {
@@ -17,16 +17,9 @@ class App extends React.PureComponent {
     });
   }
 
-  //Get info about products, when app just loaded
   componentDidMount() {
     this.props.init();
   }
-
-  showCartPopup = () => {
-    console.log(this.state.isModalOpen)
-    this.setState({isModalOpen: !this.state.isModalOpen});
-  }
-
   render() {
     return (
       <div className="App">
@@ -48,6 +41,7 @@ class App extends React.PureComponent {
         <OutsideDetector clickOutside={() => this.setState({isModalOpen: false})}>
           {this.state.isModalOpen && <CartPopup/>}
         </OutsideDetector>
+
         <Route exact path="/" component={Category}/>
         <Route path="/cart" component={CartComponent}/>
         <Route path="/product/:id" component={Product}/>
